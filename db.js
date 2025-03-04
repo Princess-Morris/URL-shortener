e3c31737e1085ee39d8c655a24a8b7e5d807ad8f
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-03-04 17:12:52 +0100

    Ref: added postgress and docker to  aplication and updated logic for post endpoing

---
+++ db.js
@@ -0,0 +1,13 @@
+// import pkg from 'pg'
+
+// const {Pool} = pkg
+
+// const pool = new Pool({
+//     host: 'db',
+//     port: 5432,
+//     user: 'princess',
+//     password: 'pass123#',
+//     database: 'pwincy'
+// })
+
+// export default pool
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import pkg from 'pg'
 
 const port = process.env.PORT || 8005
 
@@ -7,48 +8,87 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+const {Pool} = pkg
+
+const pool = new Pool({
+    user: 'myuser',
+    host: 'localhost',
+    database: 'url_shortener',
+    password: 'mypassword',
+    port: 5432
+})
+
+
 let allUrls = [
     // {id: "", originalUrl: ""}
-    // {id: 1, originalUrl: "hello there"}
-
 ]
 
 let allShortCodes = [
     // {newCode: "", original: ""}
 ]
 
+const generateShortCode = () => Math.random().toString(36).substring(2, 8)
+
 // POST /url/shorten
 app.post('/url/shorten', async (req, res) => {
-    console.log(req.body)
+    try{
+        if (!req.body.originalUrl) {
+            res.status(400).json({ message: "Please include a url" })
+        }
+    
+    
+        const urlResult = await pool.query(
+            'INSERT INTO urls (original_url) VALUES ($1) RETURNING id', [req.body.originalUrl]
+        )
 
-    if (!req.body.originalUrl) {
-        res.status(400).json({ message: "Please include a url" })
+        const urlId = urlResult.rows[0].id
+        const shortCode = generateShortCode()
+        const shortCodeUrl = `http://localhost:3000/${shortCode}`
 
+        await pool.query(
+            'INSERT INTO shortcodes (short_code, url_id) VALUES ($1, $2)', [shortCodeUrl, urlId]
+        )
+
+    
+        res.status(201).json({shortUrl: shortCodeUrl})
+
+    } catch (error){
+        console.error(error)
+        res.status(500).json({message: 'Server error'})
     }
-    console.log(req.body.originalUrl)
+    
+})
 
+// testing
+// this GET endpoint is just to see the original urls and generated urls
+app.get('/url/all', async(req, res) => {
+    
+   const result = await pool.query(
+        'SELECT * FROM shortcodes' 
+    )
 
-    const newUrl = {
-        id: allUrls.length + 1,
-        originalUrl: req.body.originalUrl
+    res.json(result.rows)
+    // res.json({allShortCodes, allUrls})
+})
+
+// GET /url/{shortCode}
+// this is to redirect the generated short url to the original url
+
+app.get('url/:shortCode', (req, res) => {
+    console.log(shortCode)
+    const shortCode = req.params.shortCode.trim()
+    const code = allShortCodes.find((aCode) => aCode.newCode === shortCode)
+
+    if (!code) {
+        res.status(400).json({ message: `this url: ${shortCode} can't be found in the db` })
+    } else {
+        // todo: will confirm from online resource whether this implementation works
+        // app.get(`${code.original}`)
+
+        res.status(200).json({ message: `you have been redirected to ${code.original}` })
     }
-
-    allUrls.push(newUrl)
-
-
-    const generateShortUrl = (url) => {
-        const newShortCode = url.replace(url, `http://localhost:3000/url/${url.slice(20, 26)}`)
-
-        // console.log(req.body.originalUrl)
-        allShortCodes.push({ newCode: newShortCode, original: url })
-        return newShortCode
-    }
-    res.status(201).json({ shortUrl: generateShortUrl(req.body.originalUrl) })
 
 })
 
-// app.get('/', (req, res) => {
-//      res.json(allUrls)
-// })
 
 app.listen(port, () => console.log(`Server has started on port ${port}`))
