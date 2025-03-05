975aee8ec82829c0faa51ca1dc657c98870dec72
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-03-05 03:46:37 +0100

    updated server with delete shortcode

--- server.js
+++ server.js
@@ -105,12 +105,12 @@ app.patch('/url/:shortCode', async (req, res) => {
 
     try {
         const shortCode = req.params.shortCode.trim()
-        const {newUrl} = req.body
-        if (!newUrl){
-            return res.status(400).json({message: "You haven't provided a url"})
+        const { newUrl } = req.body
+        if (!newUrl) {
+            return res.status(400).json({ message: "You haven't provided a url" })
         }
 
-        
+
         const result = await pool.query(`
              SELECT urls.id FROM urls
              INNER JOIN shortcodes ON urls.id = shortcodes.url_id
@@ -122,15 +122,15 @@ app.patch('/url/:shortCode', async (req, res) => {
 
         const urlId = result.rows[0].id
 
-         await pool.query(`
+        await pool.query(`
             UPDATE urls
             SET original_url = $1
             WHERE id = $2`, [newUrl, urlId])
 
-            res.json({message: `URL updated successfully for short code: ${shortCode}`})
+        res.json({ message: `URL updated successfully for short code: ${shortCode}` })
 
     } catch (error) {
-        res.status(500).json({message: `Server error`})
+        res.status(500).json({ message: `Server error` })
     }
 
 }
@@ -142,12 +142,12 @@ app.put('/url/:shortCode', async (req, res) => {
 
     try {
         const shortCode = req.params.shortCode.trim()
-        const {newUrl} = req.body
-        if (!newUrl){
-            return res.status(400).json({message: "You haven't provided a url"})
+        const { newUrl } = req.body
+        if (!newUrl) {
+            return res.status(400).json({ message: "You haven't provided a url" })
         }
 
-        
+
         const result = await pool.query(`
              SELECT urls.id FROM urls
              INNER JOIN shortcodes ON urls.id = shortcodes.url_id
@@ -159,21 +159,48 @@ app.put('/url/:shortCode', async (req, res) => {
 
         const urlId = result.rows[0].id
 
-         await pool.query(`
+        await pool.query(`
             UPDATE urls
             SET original_url = $1
             WHERE id = $2`, [newUrl, urlId])
 
-            res.json({message: `URL updated successfully for short code: ${shortCode}`})
+        res.json({ message: `URL updated successfully for short code: ${shortCode}` })
 
     } catch (error) {
-        res.status(500).json({message: `Server error`})
+        res.status(500).json({ message: `Server error` })
     }
 
 }
 )
 
+// DELETE url{shortCode}
+app.delete('/url/:shortCode', async (req, res) => {
+    try {
+        const shortCode = req.params.shortCode.trim()
 
+        const result = await pool.query(`
+        SELECT urls.id FROM urls
+        INNER JOIN shortcodes ON urls.id = shortcodes.url_id
+        WHERE shortcodes.short_code = $1`, [shortCode])
+
+        if ( result.rows.length === 0) {
+            return res.status(404).json({ message: `this shortcode ${shortCode} does not exist` })
+        }
+
+        const urlId = result.rows[0].id
+
+        await pool.query(`DELETE FROM shortcodes WHERE short_code = $1`, [shortCode])
+
+        await pool.query(`DELETE FROM urls WHERE id = $1`, [urlId])
+
+        res.json({message: `Short code ${shortCode} deleted successfully`})
+
+    } catch (error) {
+       console.log(error)
+       res.status(500).json({message: `Server error`})
+    }
+
+})
 
 
 app.listen(port, () => console.log(`Server has started on port ${port}`))
