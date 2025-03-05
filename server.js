10dc5655a1be06f7d907c9698fda93e4f2233be6
Princess-Morris <okpataprincessmorris3@gmail.com>
2025-03-05 03:17:05 +0100

    updated server with  put and patch endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-const {Pool} = pkg
+const { Pool } = pkg
 
 const pool = new Pool({
     user: 'myuser',
@@ -31,12 +31,12 @@ const generateShortCode = () => Math.random().toString(36).substring(2, 8)
 
 // POST /url/shorten
 app.post('/url/shorten', async (req, res) => {
-    try{
+    try {
         if (!req.body.originalUrl) {
             res.status(400).json({ message: "Please include a url" })
         }
-    
-    
+
+
         const urlResult = await pool.query(
             'INSERT INTO urls (original_url) VALUES ($1) RETURNING id', [req.body.originalUrl]
         )
@@ -49,22 +49,22 @@ app.post('/url/shorten', async (req, res) => {
             'INSERT INTO shortcodes (short_code, url_id) VALUES ($1, $2)', [shortCode, urlId]
         )
 
-    
-        res.status(201).json({shortUrl: shortCodeUrl})
 
-    } catch (error){
+        res.status(201).json({ shortUrl: shortCodeUrl })
+
+    } catch (error) {
         console.error(error)
-        res.status(500).json({message: 'Server error'})
+        res.status(500).json({ message: 'Server error' })
     }
-    
+
 })
 
 // testing
 // this GET endpoint is just to see the original urls and generated urls
-app.get('/url/all', async(req, res) => {
-    
-   const result = await pool.query(
-        'SELECT * FROM shortcodes' 
+app.get('/url/all', async (req, res) => {
+
+    const result = await pool.query(
+        'SELECT * FROM shortcodes'
     )
 
     res.json(result.rows)
@@ -74,31 +74,106 @@ app.get('/url/all', async(req, res) => {
 // GET /url/{shortCode}
 // this is to redirect the generated short url to the original url
 
-app.get('/url/:shortCode', async(req, res) => {
-    try{
+app.get('/url/:shortCode', async (req, res) => {
+    try {
         const shortCode = req.params.shortCode.trim()
-        console.log("Short code received:", shortCode)
-    
-           const result = await pool.query(
+        // console.log("Short code received:", shortCode)
+
+        const result = await pool.query(
             `SELECT original_url FROM urls
             INNER JOIN shortcodes ON urls.id = shortcodes.url_id
             WHERE shortcodes.short_code = $1`, [shortCode]
-           )
+        )
 
-           console.log("Databas result:", result.rows)
+        //    console.log("Databas result:", result.rows)
 
-        if (result.rows.length === 0){
-            return res.status(404).json({message: 'provide an existing short url'})
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: 'provide an existing short url' })
         }
 
         // res.json({redirectTo: result.rows[0].original_url})
         res.redirect(result.rows[0].original_url)
-    } catch(error){
-         console.error(error)
-         res.status(500).json({message: `Server error`})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: `Server error` })
     }
 
 })
+
+// PATCH /url/{shortCode}
+app.patch('/url/:shortCode', async (req, res) => {
+
+    try {
+        const shortCode = req.params.shortCode.trim()
+        const {newUrl} = req.body
+        if (!newUrl){
+            return res.status(400).json({message: "You haven't provided a url"})
+        }
+
+        
+        const result = await pool.query(`
+             SELECT urls.id FROM urls
+             INNER JOIN shortcodes ON urls.id = shortcodes.url_id
+             WHERE shortcodes.short_code = $1`, [shortCode])
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: `this ${shortCode} does not exist on the db` })
+        }
+
+        const urlId = result.rows[0].id
+
+         await pool.query(`
+            UPDATE urls
+            SET original_url = $1
+            WHERE id = $2`, [newUrl, urlId])
+
+            res.json({message: `URL updated successfully for short code: ${shortCode}`})
+
+    } catch (error) {
+        res.status(500).json({message: `Server error`})
+    }
+
+}
+)
+
+
+// PUT /url/{shortCode}
+app.put('/url/:shortCode', async (req, res) => {
+
+    try {
+        const shortCode = req.params.shortCode.trim()
+        const {newUrl} = req.body
+        if (!newUrl){
+            return res.status(400).json({message: "You haven't provided a url"})
+        }
+
+        
+        const result = await pool.query(`
+             SELECT urls.id FROM urls
+             INNER JOIN shortcodes ON urls.id = shortcodes.url_id
+             WHERE shortcodes.short_code = $1`, [shortCode])
+
+        if (result.rows.length === 0) {
+            return res.status(404).json({ message: `this ${shortCode} does not exist on the db` })
+        }
+
+        const urlId = result.rows[0].id
+
+         await pool.query(`
+            UPDATE urls
+            SET original_url = $1
+            WHERE id = $2`, [newUrl, urlId])
+
+            res.json({message: `URL updated successfully for short code: ${shortCode}`})
+
+    } catch (error) {
+        res.status(500).json({message: `Server error`})
+    }
+
+}
+)
+
+
 
 
 app.listen(port, () => console.log(`Server has started on port ${port}`))
